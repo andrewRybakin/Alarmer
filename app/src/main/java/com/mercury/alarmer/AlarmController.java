0db81a7daa942c143d4d87f45b33ee1914de673e
andrewRybakin <drnraw@yandex.ru>
2015-11-24 22:44:35 +0400

    Initial commit. Вроде все работает

---
+++ app/src/main/java/com/mercury/alarmer/AlarmController.java
@@ -0,0 +1,196 @@
+package com.mercury.alarmer;
+
+import android.app.AlarmManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.PowerManager;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Locale;
+
+public class AlarmController {
+
+    public static final String PREFERENCES = "MainPrefs";
+    public static final String PREF_TITLE = "MainPrefs.title";
+    public static final String PREF_DESCRIPTION = "MainPrefs.description";
+    public static final String PREF_YEAR = "MainPrefs.year";
+    public static final String PREF_MONTH = "MainPrefs.month";
+    public static final String PREF_DAY = "MainPrefs.day";
+    public static final String PREF_HOUR = "MainPrefs.hour";
+    public static final String PREF_MINUTE = "MainPrefs.minute";
+    public static final String PREF_SET = "MainPrefs.isSet";
+
+    private static AlarmController instance;
+    private Calendar calendar;
+    private static Context context;
+    private String title, description;
+    private SharedPreferences preferences;
+    private boolean alarmSet;
+
+    private AlarmController(Context c) {
+        calendar = Calendar.getInstance();
+        preferences = c.getSharedPreferences(PREFERENCES, Context.MODE_PRIVATE);
+        title = preferences.getString(PREF_TITLE, "");
+        description = preferences.getString(PREF_DESCRIPTION, "");
+        int year, month, day, hour, minute;
+        year = preferences.getInt(PREF_YEAR, getYear());
+        month = preferences.getInt(PREF_MONTH, getMonth());
+        day = preferences.getInt(PREF_DAY, getDay());
+        hour = preferences.getInt(PREF_HOUR, getHour());
+        minute = preferences.getInt(PREF_MINUTE, getMinute());
+        alarmSet = preferences.getBoolean(PREF_SET, false);
+        setDate(year, month, day);
+        setTime(hour, minute);
+    }
+
+    public static AlarmController getInstance(Context c) {
+        if (instance == null)
+            instance = new AlarmController(c);
+        context = c;
+        return instance;
+    }
+
+    private String getMonthName(int month) {
+        Calendar cal1 = Calendar.getInstance();
+        cal1.set(Calendar.MONTH, month);
+        return cal1.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.getDefault());
+    }
+
+    public int getYear() {
+        return calendar.get(Calendar.YEAR);
+    }
+
+    public int getMonth() {
+        return calendar.get(Calendar.MONTH);
+    }
+
+    public int getDay() {
+        return calendar.get(Calendar.DAY_OF_MONTH);
+    }
+
+    public int getHour() {
+        return calendar.get(Calendar.HOUR_OF_DAY);
+    }
+
+    public int getMinute() {
+        return calendar.get(Calendar.MINUTE);
+    }
+
+    public String getDate() {
+        return String.format("%d %s %d",
+                calendar.get(Calendar.DAY_OF_MONTH),
+                getMonthName(calendar.get(Calendar.MONTH)),
+                calendar.get(Calendar.YEAR)
+        );
+    }
+
+    public String getTime() {
+        return String.format("%d:%d",
+                calendar.get(Calendar.HOUR_OF_DAY),
+                calendar.get(Calendar.MINUTE)
+        );
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setTime(int hour, int minute) {
+        calendar.set(Calendar.HOUR_OF_DAY, hour);
+        calendar.set(Calendar.MINUTE, minute);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+    }
+
+    private void saveChanges() {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putInt(PREF_HOUR, getHour());
+        editor.putInt(PREF_MINUTE, getMinute());
+        editor.putInt(PREF_YEAR, getYear());
+        editor.putInt(PREF_MONTH, getMonth());
+        editor.putInt(PREF_DAY, getDay());
+        editor.putString(PREF_TITLE, getTitle());
+        editor.putString(PREF_DESCRIPTION, getDescription());
+        editor.putBoolean(PREF_SET, alarmSet);
+        editor.apply();
+    }
+
+    public void setDate(int year, int month, int day) {
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.YEAR, year);
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isAlarmSet() {
+        return alarmSet;
+    }
+
+    public void setAlarm() {
+        alarmSet = true;
+        saveChanges();
+        AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, AlarmReceiver.class);
+        PendingIntent alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+        alarmMgr.set(AlarmManager.RTC_WAKEUP,
+                calendar.getTimeInMillis(), alarmIntent);
+        Toast.makeText(context, "Notification created!", Toast.LENGTH_LONG).show();
+    }
+
+    public static class AlarmReceiver extends BroadcastReceiver {
+
+        public AlarmReceiver() {
+            super();
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d("Alarm", "Oh Lord Jesus! It's a broadcast!!!");
+            PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+            PowerManager.WakeLock wakeLock = pm.newWakeLock((PowerManager.SCREEN_BRIGHT_WAKE_LOCK | PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP), "TAG");
+            wakeLock.acquire();
+            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context);
+            mBuilder.setSmallIcon(android.R.drawable.sym_action_email);
+            AlarmController controller = AlarmController.getInstance(context);
+            mBuilder.setContentTitle(controller.getTitle());
+            mBuilder.setContentText(controller.getDescription());
+            Intent i = new Intent(context, MainActivity.class);
+            PendingIntent pI = PendingIntent.getActivity(context, 0, i, 0);
+            mBuilder.setContentIntent(pI);
+            mBuilder.setAutoCancel(true);
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.notify(1, mBuilder.build());
+            context.getSharedPreferences(PREFERENCES, Context.MODE_PRIVATE).edit().clear().apply();
+            wakeLock.release();
+        }
+    }
+
+    public boolean isTimeExpired() {
+        return (calendar.getTimeInMillis() < System.currentTimeMillis());
+    }
+
+    public void reset() {
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        title = "";
+        description = "";
+        alarmSet = false;
+    }
+}
---
+++ app/src/main/java/com/mercury/alarmer/MainActivity.java
@@ -0,0 +1,117 @@
+package com.mercury.alarmer;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TimePickerDialog timePickerDialog;
+    private DatePickerDialog datePickerDialog;
+    private Button date, time, save;
+    private AlarmController alarmController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText title = (EditText) findViewById(R.id.title);
+        EditText description = (EditText) findViewById(R.id.description);
+        date = (Button) findViewById(R.id.date_button);
+        time = (Button) findViewById(R.id.time_button);
+        save = (Button) findViewById(R.id.save_button);
+        alarmController = AlarmController.getInstance(this);
+        if (alarmController.isTimeExpired())
+            alarmController.reset();
+        datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                alarmController.setDate(year, monthOfYear, dayOfMonth);
+                date.setText(alarmController.getDate());
+            }
+        }, alarmController.getYear(), alarmController.getMonth(), alarmController.getDay());
+        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis());
+        timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                alarmController.setTime(hourOfDay, minute);
+                time.setText(alarmController.getTime());
+            }
+        }, alarmController.getHour(), alarmController.getMinute(), true);
+        date.setOnClickListener(this);
+        time.setOnClickListener(this);
+        save.setOnClickListener(this);
+        title.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                alarmController.setTitle(s.toString());
+            }
+        });
+        description.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                alarmController.setDescription(s.toString());
+            }
+        });
+        date.setText(alarmController.getDate());
+        time.setText(alarmController.getTime());
+        title.setText(alarmController.getTitle());
+        description.setText(alarmController.getDescription());
+        if (alarmController.isAlarmSet())
+            save.setText(R.string.update);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.date_button) {
+            datePickerDialog.show();
+        } else if (id == R.id.time_button) {
+            timePickerDialog.show();
+        } else if (id == R.id.save_button) {
+            alarmController.setAlarm();
+            save.setText(R.string.update);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
+
+
+
